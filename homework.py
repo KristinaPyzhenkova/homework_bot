06cff6e002b022d6dd0cb3512d0727c68820c44b
Kristina Pyzhenkova <pyzhenkova-k@mail.ru>
2022-03-13 15:27:59 +0300

    second

--- homework.py
+++ homework.py
@@ -1,24 +1,26 @@
-from http import HTTPStatus
-import logging
-
 import os
 import time
-from xmlrpc.client import ResponseError
+import logging
+import sys
 
 import requests
 
+from http import HTTPStatus
+from xmlrpc.client import ResponseError
+
+import telegram
 from telegram import Bot
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='program.log',
-    format='%(asctime)s, %(levelname)s, %(message)s'
-)
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='program.log',
+        format='%(asctime)s, %(levelname)s, %(message)s'
+    )
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -27,12 +29,10 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 0
+RETRY_TIME = 600
 
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-payload = {'from_date': 1636992730}
-status = None
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -43,7 +43,17 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message):
     """Отправляет сообщение в Telegram чат."""
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    sent_message = bot.send_message(TELEGRAM_CHAT_ID, message)
+    if not sent_message:
+        raise telegram.TelegramError(f'Ошибка при отправке: {message}')
+    else:
+        logger.info('Сообщение отправлено.')
+
+
+class Impermissibility_Endpoint(Exception):
+    """Ошибка недоступность эндпоинта."""
+
+    pass
 
 
 def get_api_answer(current_timestamp):
@@ -54,7 +64,7 @@ def get_api_answer(current_timestamp):
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
         if response.status_code != HTTPStatus.OK:
             logger.error('Недоступность эндпоинта.')
-            raise ResponseError('Недоступность эндпоинта.')
+            raise Impermissibility_Endpoint('Недоступность эндпоинта.')
         return response.json()
     except Exception:
         logger.error('Ошибка при запросе.')
@@ -63,9 +73,10 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
-    if isinstance(response, list) or isinstance(
-        response.get('homeworks'), dict
-    ) or response == {} or response.get('homeworks') is None:
+    if isinstance(response, list) or response == {}:
+        logger.error('Отсутствие ожидаемых ключей в ответе API.')
+        raise TypeError('Отсутствие ожидаемых ключей в ответе API.')
+    if isinstance(response.get('homeworks'), dict):
         logger.error('Отсутствие ожидаемых ключей в ответе API.')
         raise TypeError('Отсутствие ожидаемых ключей в ответе API.')
 
@@ -76,10 +87,14 @@ def check_response(response):
     return homeworks
 
 
+class Verdict_is_none(Exception):
+    """Ошибка статус неизвестен."""
+
+    pass
+
+
 def parse_status(homework: dict):
     """Извлекает из инф. о домашней работе статус."""
-    global status
-
     homework_status = homework.get('status')
     homework_name = homework.get('homework_name')
 
@@ -88,46 +103,42 @@ def parse_status(homework: dict):
 
     verdict = HOMEWORK_STATUSES.get(homework_status)
     if verdict is None:
-        raise Exception('Статус неизвестен.')
+        raise Verdict_is_none('Статус неизвестен.')
 
     message = 'Изменился статус проверки работы'
-
-    if verdict != status:
-        status = verdict
-        return f'{message} "{homework_name}". {verdict}'
+    return f'{message} "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверяет доступность переменных окружения."""
     try:
-        if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-            return True
+        return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
     except Exception:
         logger.critical('Отсутствие обязательных переменных.')
-        raise AssertionError('Отсутствие обязательных переменных.')
+        sys.exit('Отсутствие обязательных переменных.')
 
 
 def main():
     """Основная логика работы бота."""
     bot = Bot(token=TELEGRAM_TOKEN)
+    status = None
+    two_week = 86400 * 14
+    current_timestamp = int(time.time()) - two_week
     while check_tokens():
         try:
-            current_timestamp = 1636992730
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
+            if status != homework[0].get('status'):
+                send_message(bot, parse_status(homework[0]))
+                status = homework[0].get('status')
+            else:
+                logger.debug('Отсутствие в ответе новых статусов.')
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
             logger.error('Cбой при отправке сообщения в Telegram.')
             time.sleep(RETRY_TIME)
-        else:
-            message = parse_status(homework[0])
-            if message is not None:
-                send_message(bot, message)
-                logger.info('Сообщение отправлено.')
-            else:
-                logger.debug('Отсутствие в ответе новых статусов.')
 
 
 if __name__ == '__main__':
